26129d2f4b399b4385ef445bf12428a24e8c5b75
Kane <5062932+kanewilliams@users.noreply.github.com>
2024-09-10 10:37:50 +1200

    Initial commit

---
+++ src/packages.R
@@ -0,0 +1,25 @@
+suppressPackageStartupMessages({
+
+  # ADMIN
+  library(conflicted) # alerts of conflicts between packages
+  library(formatR)
+  library(readr)
+  
+  # TIDYVERSE/LAYOUT
+  library(tidyverse)
+  library(ggplot2)
+  library(dplyr)
+  library(patchwork) # for 2x2 grids
+  library(gridExtra)
+  library(scales) # Human-readable y-axis values
+  
+  # TIME SERIES
+  library(forecast)  
+  library(tsibble) # temporal data for tidyverse
+  library(feasts) # for gg_lag
+  library(lubridate) # for Date Processing
+  
+  
+  
+  
+})
